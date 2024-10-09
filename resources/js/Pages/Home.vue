8a1f974e26803b9e6f7ed9bed2f0a98b1e7db067
frenchiejnr <frenchiejnr@gmail.com>
2024-10-09 17:14:25 +0100

    feat: Halachic Times formatting

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -205,10 +205,53 @@ const zmanim = [
     <h1 class="text-3xl">Home</h1>
     <div>
         <h2>{{ date }}</h2>
-        <ul>
-            <li v-for="zman in zmanim" :key="zman.name">
-                {{ zman.name }} : {{ zman.time }} : {{ zman.hebrewName }}
-            </li>
-        </ul>
+        <div class="flex flex-col">
+            <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div
+                    class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                    <div
+                        class="overflow-hidden border-b border-gray-200 shadow sm:rounded-lg">
+                        <table class="min-w-full divide-y divide-gray-200">
+                            <tbody class="divide-y divide-gray-200 bg-white">
+                                <tr
+                                    v-for="zman in zmanim"
+                                    :key="zman.name"
+                                    class="flex items-center justify-between text-sm font-medium text-gray-900">
+                                    <td
+                                        class="hidden basis-2/5 whitespace-nowrap px-6 py-4 sm:block">
+                                        <p>
+                                            {{ zman.name }}
+                                        </p>
+                                    </td>
+                                    <td
+                                        class="hidden whitespace-nowrap px-6 py-4 sm:block">
+                                        <p>
+                                            {{ zman.time }}
+                                        </p>
+                                    </td>
+                                    <td
+                                        class="hidden basis-2/5 whitespace-nowrap px-6 py-4 text-right sm:block">
+                                        {{ zman.hebrewName }}
+                                    </td>
+                                    <td
+                                        class="block whitespace-nowrap px-6 py-4 sm:hidden">
+                                        <p>
+                                            {{ zman.hebrewName }}
+                                        </p>
+                                        <p>
+                                            {{ zman.name }}
+                                        </p>
+                                    </td>
+                                    <td
+                                        class="block whitespace-nowrap px-6 py-4 sm:hidden">
+                                        {{ zman.time }}
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
