b4f6765c2c3e94ca4bc0bb8f8c65f05dd80bea36
frenchiejnr <frenchiejnr@gmail.com>
2024-10-09 10:40:41 +0100

    feat: Hebrew Date

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,5 +1,33 @@
 <script setup>
 import { Head } from "@inertiajs/vue3";
+import { ref } from "vue";
+const today = new Date().toISOString().split("T")[0];
+
+const isLoading = ref(true);
+const date = ref("null");
+
+const fetchDate = async () => {
+    const response = await fetch(
+        `https://www.hebcal.com/converter?cfg=json&date=${today}&g2h=1&strict=1`
+    );
+    return await response.json();
+};
+
+const convertDate = (data) => {
+    return `${data.hd} ${data.hm} ${data.hy}`;
+};
+
+const getDate = async () => {
+    try {
+        isLoading.value = true;
+        const data = await fetchDate();
+        date.value = convertDate(data);
+        isLoading.value = false;
+    } catch (error) {
+        console.error(error);
+    }
+};
+getDate();
 </script>
 
 <template>
@@ -10,4 +38,12 @@ import { Head } from "@inertiajs/vue3";
             head-key="description" />
     </Head>
     <h1 class="text-3xl">Home</h1>
+    <div>
+        <h2>{{ date }}</h2>
+        <ul>
+            <li v-for="zman in zmanim" :key="zman.name">
+                {{ zman.name }}: {{ zman.time }}
+            </li>
+        </ul>
+    </div>
 </template>
