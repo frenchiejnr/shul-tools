d55454cf946f9b0ee5fd28d996e8b166148e6e27
frenchiejnr <frenchiejnr@gmail.com>
2024-12-17 16:19:17 +0000

    feat: add a new setting

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Setting;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Request;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\Auth;
+
 
 use App\Models\SettingsKeys;
 
@@ -12,12 +14,25 @@ class SettingsController extends Controller
 {
     public function index(Request $request)
     {
-        $tenantId = $request->user()->tenant_id;
-        $settings = Setting::where('tenant_id', $tenantId)->get();
+        $tenant_id = Auth::user()->tenant_id;
+        $settings = Setting::where('tenant_id', $tenant_id)->get();
         $settingsKeys = SettingsKeys::whereNotIn('key', $settings->pluck('key'))->get();
         return Inertia::render('Settings/Index', [
             'settings' => $settings,
             'settingsKeys' => $settingsKeys
         ]);
+    }
+
+    public function store()
+    {
+        $tenantId = Auth::user()->tenant_id;
+        $data = Request::validate([
+            'key' => ['required'],
+            'value' => ['required'],
+        ]);
+        $setting = new Setting($data);
+        $setting->tenant_id = $tenantId;
+        $setting->save();
+        return redirect('/settings');
     }
 }
--- resources/js/Pages/Settings/Index.vue
+++ resources/js/Pages/Settings/Index.vue
@@ -1,9 +1,23 @@
 <script setup>
-import { Head } from "@inertiajs/vue3";
+import { Head, useForm } from "@inertiajs/vue3";
 let props = defineProps({
     settings: Object,
     settingsKeys: Object,
 });
+
+let form = useForm({
+    key: "",
+    value: "",
+    tenant: "",
+});
+
+let submit = () => {
+    form.post("/settings", {
+        onSuccess: () => {
+            form.reset();
+        },
+    });
+};
 </script>
 
 <template>
@@ -14,9 +28,27 @@ let props = defineProps({
             {{ setting.key }}: {{ setting.value }}
         </li>
     </ul>
-    <select name="key" id="key">
-        <option v-for="key in settingsKeys" :key="key.id" :value="key.key">
-            {{ key.label }}
-        </option>
-    </select>
+    <!-- TODO Only show if admin -->
+    <form @submit.prevent="submit">
+        <div class="flex items-center">
+            <select v-model="form.key" name="key" id="key" class="mr-2">
+                <option
+                    v-for="key in settingsKeys"
+                    :key="key.id"
+                    :value="key.key">
+                    {{ key.label }}
+                </option>
+            </select>
+            <input
+                type="text"
+                v-model="form.value"
+                placeholder="Enter value"
+                class="mr-2" />
+            <button
+                type="submit"
+                class="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600">
+                Save
+            </button>
+        </div>
+    </form>
 </template>
