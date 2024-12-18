30dca5f68f7c186c0b03a1ea2dcaaf61f0eef9bf
frenchiejnr <frenchiejnr@gmail.com>
2024-12-18 14:25:22 +0000

    refactor: SettingsList and SettingsAdd

---
+++ resources/js/Pages/Settings/Components/SettingsList.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import Table from "../../../Shared/Table.vue";
+import TableRowData from "../../../Shared/TableRowData.vue";
+defineProps<{
+    settings: Record<string, any> | undefined;
+}>();
+</script>
+
+<template>
+    <Table>
+        <template #heading>
+            <h1 class="text-3xl">Settings</h1>
+        </template>
+        <template #rows>
+            <tr v-for="setting in settings" :key="setting.id">
+                <TableRowData>
+                    <div class="text-sm font-medium text-gray-900">
+                        {{ setting.key }}
+                    </div>
+                </TableRowData>
+                <TableRowData :right-aligned="true">
+                    <div class="text-sm font-medium text-gray-900">
+                        {{ setting.value }}
+                    </div>
+                </TableRowData>
+            </tr>
+        </template>
+    </Table>
+</template>
--- resources/js/Pages/Settings/Index.vue
+++ resources/js/Pages/Settings/Index.vue
@@ -1,11 +1,10 @@
 <script setup>
 import { Head, useForm } from "@inertiajs/vue3";
-import Table from "../../Shared/Table.vue";
-import TableRowData from "../../Shared/TableRowData.vue";
+import SettingsList from "./Components/SettingsList.vue";
+import SettingsAddSetting from "./Components/SettingsAdd.vue";
 let props = defineProps({
     settings: Object,
     settingsKeys: Object,
-    can: Object,
 });
 
 let form = useForm({
@@ -25,59 +24,9 @@ let submit = () => {
 
 <template>
     <Head title="Settings"></Head>
-    <div class="mx-auto max-w-3xl">
-        <div></div>
-    </div>
-    <Table>
-        <template #heading>
-            <h1 class="text-3xl">Settings</h1>
-        </template>
-        <template #rows>
-            <tr v-for="setting in settings" :key="setting.id">
-                <TableRowData>
-                    <div class="text-sm font-medium text-gray-900">
-                        {{ setting.key }}
-                    </div>
-                </TableRowData>
-                <TableRowData :right-aligned="true">
-                    <div class="text-sm font-medium text-gray-900">
-                        {{ setting.value }}
-                    </div>
-                </TableRowData>
-            </tr>
-        </template>
-    </Table>
-    <Table v-if="props.settingsKeys.length">
-        <template #rows>
-            <TableRowData>
-                <form @submit.prevent="submit" class="w-full">
-                    <div class="flex justify-between">
-                        <select
-                            v-model="form.key"
-                            name="key"
-                            id="key"
-                            class="mr-2 w-1/2">
-                            <option value="" disabled>Select setting</option>
-                            <option
-                                v-for="key in settingsKeys"
-                                :key="key.id"
-                                :value="key.key">
-                                {{ key.label }}
-                            </option>
-                        </select>
-                        <input
-                            type="text"
-                            v-model="form.value"
-                            placeholder="Enter value"
-                            class="mr-2 w-1/2" />
-                        <button
-                            type="submit"
-                            class="w-1/4 rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600">
-                            Save
-                        </button>
-                    </div>
-                </form>
-            </TableRowData>
-        </template>
-    </Table>
+    <SettingsList :settings="settings" />
+    <SettingsAddSetting
+        :submit="submit"
+        :form="form"
+        :settingsKeys="settingsKeys" />
 </template>
