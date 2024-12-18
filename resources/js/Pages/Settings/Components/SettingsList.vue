67deb67c0d24660f8c02c9e753f1c3ad03f0f8d0
frenchiejnr <frenchiejnr@gmail.com>
2024-12-18 17:23:19 +0000

    feat: add an edit button feat: when clicked, have a submit and cancel button

--- resources/js/Pages/Settings/Components/SettingsList.vue
+++ resources/js/Pages/Settings/Components/SettingsList.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Table from "../../../Shared/Table.vue";
 import TableRowData from "../../../Shared/TableRowData.vue";
+import { ref } from "vue";
 defineProps<{
     settings: Record<string, any> | undefined;
 }>();
+const isEditing = ref({});
 </script>
 
 <template>
@@ -12,16 +14,34 @@ defineProps<{
             <h1 class="text-3xl">Settings</h1>
         </template>
         <template #rows>
-            <tr v-for="setting in settings" :key="setting.id">
-                <TableRowData>
+            <tr
+                v-for="setting in settings"
+                :key="setting.id"
+                class="flex items-center justify-between">
+                <TableRowData class="basis-3/12">
                     <div class="text-sm font-medium text-gray-900">
                         {{ setting.key }}
                     </div>
                 </TableRowData>
-                <TableRowData :right-aligned="true">
-                    <div class="text-sm font-medium text-gray-900">
-                        {{ setting.value }}
-                    </div>
+                <TableRowData class="grow">
+                    <input
+                        class="w-full rounded-md border-2 border-gray-300 px-2 py-1 text-sm font-medium text-gray-900"
+                        :value="setting.value"
+                        :disabled="!isEditing[setting.id]"
+                        @input="isEditing[setting.id] = true" />
+                </TableRowData>
+                <TableRowData class="" :right-aligned="true">
+                    <button
+                        class="text-indigo-600 hover:text-indigo-900"
+                        @click="isEditing[setting.id] = !isEditing[setting.id]">
+                        {{ isEditing[setting.id] ? "Save" : "Edit" }}
+                    </button>
+                    <button
+                        v-if="isEditing[setting.id]"
+                        class="ml-2 text-red-600 hover:text-red-900"
+                        @click="isEditing[setting.id] = false">
+                        Cancel
+                    </button>
                 </TableRowData>
             </tr>
         </template>
