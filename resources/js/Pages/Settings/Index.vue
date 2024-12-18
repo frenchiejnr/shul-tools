1ded71899d9342571b8186a3f7edd6908992c22a
frenchiejnr <frenchiejnr@gmail.com>
2024-12-18 11:39:39 +0000

    feat: style the settings screen

--- resources/js/Pages/Settings/Index.vue
+++ resources/js/Pages/Settings/Index.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { Head, useForm } from "@inertiajs/vue3";
+import Table from "../../Shared/Table.vue";
+import TableRowData from "../../Shared/TableRowData.vue";
 let props = defineProps({
     settings: Object,
     settingsKeys: Object,
@@ -23,33 +25,59 @@ let submit = () => {
 
 <template>
     <Head title="Settings"></Head>
-    <h1 class="text-3xl">Settings</h1>
-    <ul>
-        <li v-for="setting in settings" :key="setting.id">
-            {{ setting.key }}: {{ setting.value }}
-        </li>
-    </ul>
-    <!-- TODO Only show if admin -->
-    <form @submit.prevent="submit" v-if="can.addSetting">
-        <div class="flex items-center">
-            <select v-model="form.key" name="key" id="key" class="mr-2">
-                <option
-                    v-for="key in settingsKeys"
-                    :key="key.id"
-                    :value="key.key">
-                    {{ key.label }}
-                </option>
-            </select>
-            <input
-                type="text"
-                v-model="form.value"
-                placeholder="Enter value"
-                class="mr-2" />
-            <button
-                type="submit"
-                class="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600">
-                Save
-            </button>
-        </div>
-    </form>
+    <div class="mx-auto max-w-3xl">
+        <div></div>
+    </div>
+    <Table>
+        <template #heading>
+            <h1 class="text-3xl">Settings</h1>
+        </template>
+        <template #rows>
+            <tr v-for="setting in settings" :key="setting.id">
+                <TableRowData>
+                    <div class="text-sm font-medium text-gray-900">
+                        {{ setting.key }}
+                    </div>
+                </TableRowData>
+                <TableRowData :right-aligned="true">
+                    <div class="text-sm font-medium text-gray-900">
+                        {{ setting.value }}
+                    </div>
+                </TableRowData>
+            </tr>
+        </template>
+    </Table>
+    <Table>
+        <template #rows>
+            <TableRowData>
+                <form @submit.prevent="submit" class="w-full">
+                    <div class="flex justify-between">
+                        <select
+                            v-model="form.key"
+                            name="key"
+                            id="key"
+                            class="mr-2 w-1/2">
+                            <option value="" disabled>Select setting</option>
+                            <option
+                                v-for="key in settingsKeys"
+                                :key="key.id"
+                                :value="key.key">
+                                {{ key.label }}
+                            </option>
+                        </select>
+                        <input
+                            type="text"
+                            v-model="form.value"
+                            placeholder="Enter value"
+                            class="mr-2 w-1/2" />
+                        <button
+                            type="submit"
+                            class="w-1/4 rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600">
+                            Save
+                        </button>
+                    </div>
+                </form>
+            </TableRowData>
+        </template>
+    </Table>
 </template>
---
+++ resources/js/Shared/TableRowData.vue
@@ -0,0 +1,13 @@
+<script setup>
+const props = defineProps({
+    rightAligned: Boolean,
+});
+</script>
+
+<template>
+    <td class="whitespace-nowrap px-1 py-4 sm:px-6">
+        <div class="flex items-center" :class="{ 'justify-end': rightAligned }">
+            <slot />
+        </div>
+    </td>
+</template>
