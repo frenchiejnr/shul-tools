adbb382d5d238e433d47e217a672b8552b83f086
frenchiejnr <frenchiejnr@gmail.com>
2025-01-06 14:44:53 +0000

    feat: theme for small screen

--- resources/js/Pages/Settings/Components/SettingsRow.vue
+++ resources/js/Pages/Settings/Components/SettingsRow.vue
@@ -36,43 +36,51 @@ let deleteSetting = (setting) => {
 </script>
 
 <template>
-    <tr :key="setting.id" class="flex items-center justify-between">
-        <TableRowData class="basis-3/12">
-            <input
-                class="text-sm font-medium text-gray-900"
-                v-model="setting.key" />
-        </TableRowData>
-        <TableRowData class="grow">
-            <input
-                class="w-full rounded-md border-2 border-gray-300 px-2 py-1 text-sm font-medium text-gray-900"
-                v-model="setting.value"
-                :disabled="!isEditing[setting.id]" />
-        </TableRowData>
+    <div :key="setting.id" class="flex items-center justify-between">
+        <div
+            class="flex flex-grow flex-col justify-between sm:flex-row sm:items-center">
+            <TableRowData class="basis-3/12 self-center sm:self-start">
+                <input
+                    class="text-center text-sm font-medium text-gray-900 sm:text-left"
+                    v-model="setting.key" />
+            </TableRowData>
+            <TableRowData class="grow">
+                <input
+                    class="w-full rounded-md border-2 border-gray-300 px-2 py-1 text-sm font-medium text-gray-900"
+                    v-model="setting.value"
+                    :disabled="!isEditing[setting.id]" />
+            </TableRowData>
+        </div>
         <TableRowData class="" :right-aligned="true">
-            <button
-                v-if="!isEditing[setting.id]"
-                class="text-indigo-600 hover:text-indigo-900"
-                @click="startEdit(setting)">
-                Edit
-            </button>
-            <button
-                v-if="isEditing[setting.id]"
-                class="text-indigo-600 hover:text-indigo-900"
-                @click="submitEdit(setting), (isEditing[setting.id] = false)">
-                Save
-            </button>
-            <button
-                v-if="isEditing[setting.id]"
-                class="ml-2 text-red-600 hover:text-red-900"
-                @click="cancelEdit(setting)">
-                Cancel
-            </button>
-            <button
-                v-if="!isEditing[setting.id]"
-                class="ml-2 text-red-600 hover:text-red-900"
-                @click="deleteSetting(setting)">
-                Delete
-            </button>
+            <div
+                class="flex flex-col justify-between sm:flex-row sm:items-center">
+                <button
+                    v-if="!isEditing[setting.id]"
+                    class="text-indigo-600 hover:text-indigo-900"
+                    @click="startEdit(setting)">
+                    Edit
+                </button>
+                <button
+                    v-if="isEditing[setting.id]"
+                    class="text-indigo-600 hover:text-indigo-900"
+                    @click="
+                        (submitEdit(setting), (isEditing[setting.id] = false))
+                    ">
+                    Save
+                </button>
+                <button
+                    v-if="isEditing[setting.id]"
+                    class="ml-2 text-red-600 hover:text-red-900"
+                    @click="cancelEdit(setting)">
+                    Cancel
+                </button>
+                <button
+                    v-if="!isEditing[setting.id]"
+                    class="ml-2 text-red-600 hover:text-red-900"
+                    @click="deleteSetting(setting)">
+                    Delete
+                </button>
+            </div>
         </TableRowData>
-    </tr>
+    </div>
 </template>
--- resources/js/Shared/TableRowData.vue
+++ resources/js/Shared/TableRowData.vue
@@ -5,7 +5,7 @@ const props = defineProps({
 </script>
 
 <template>
-    <td class="whitespace-nowrap px-1 py-4 sm:px-6">
+    <td class="whitespace-nowrap px-1 py-1 sm:px-6 sm:py-4">
         <div class="flex items-center" :class="{ 'justify-end': rightAligned }">
             <slot />
         </div>
