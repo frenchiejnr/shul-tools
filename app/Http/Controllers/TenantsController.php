c84c3b380149588b05f69fb563c910dd131ac415
frenchiejnr <frenchiejnr@gmail.com>
2025-01-14 11:58:24 +0000

    feat: edit a tenant

--- app/Http/Controllers/TenantsController.php
+++ app/Http/Controllers/TenantsController.php
@@ -3,6 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Models\Tenant;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Request;
 
-class TenantsController extends Controller {}
+class TenantsController extends Controller
+{
+
+
+    public function edit(int $tenantId)
+    {
+        $tenant = Tenant::findOrFail($tenantId);
+        $data = Request::validate([
+            'name' => ['required', 'max:255'],
+            'domain' => ['required', 'max:255'],
+        ]);
+        $tenant->update($data);
+    }
+}
